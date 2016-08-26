e02f0d451cbd22181bc0e00eb528cf423c9c3ab5
Ara Yeressian <ayeressian2@gmail.com>
2016-08-26 15:57:37 +0400

    .

--- index.js
+++ index.js
@@ -1,5 +1,64 @@
 'use strict';
 
-class MusicProvider {}
+class MusicProvider {
+    completeMissing(asset) {
+        const URI_BEGINNING = 'http://img.youtube.com/vi/';
+        const TIME_OUT = 500;
+
+        if (!asset.backgroundImageUrl) {
+            let youtubeId = asset.url.substr(id.length - 11);
+
+            let url = `${URI_BEGINNING}${youtubeId}/maxresdefault.jpg`;
+            return rp.get({
+                url
+            }).then(() => {
+                asset.backgroundImageUrl = url;
+            }).catch(err => {
+                if (err.statusCode === 404) {
+                    url = `${URI_BEGINNING}${youtubeId}/hqdefault.jpg`;
+                    return rp.get({
+                        url,
+                        timeout: TIME_OUT
+                    }).then(() => {
+                        asset.backgroundImageUrl = url;
+                    }).catch(err => {
+                        if (err.statusCode === 404) {
+                            url = `${URI_BEGINNING}${youtubeId}/mqdefault.jpg`;
+                            return rp.get({
+                                url,
+                                timeout: TIME_OUT
+                            }).then(() => {
+                                asset.backgroundImageUrl = url;
+                            }).catch(err => {
+                                if (err.statusCode === 404) {
+                                    url = `${URI_BEGINNING}${youtubeId}/sddefault.jpg`;
+                                    return rp.get({
+                                        url,
+                                        timeout: TIME_OUT
+                                    }).then(() => {
+                                        asset.backgroundImageUrl = url;
+                                    }).catch(err => {
+                                        if (err.statusCode === 404) {
+                                            console.log(`${URI_BEGINNING}${youtubeId}/maxresdefault.jpg`);
+                                        } else {
+                                            throw err;
+                                        }
+                                    });
+                                } else {
+                                    throw err;
+                                }
+                            });
+                        } else {
+                            throw err;
+                        }
+                    });
+                } else {
+                    throw err;
+                }
+            }).catch(console.error);
+        }
+        return Promise.resolve(asset);
+    }
+}
 
 module.exports = MusicProvider;
