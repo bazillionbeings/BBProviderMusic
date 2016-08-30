5989513c221742b1b7b6ca2b4054c4875917662d
Ara Yeressian <ayeressian2@gmail.com>
2016-08-30 13:31:54 +0400

    .

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const rp = require('request-promise'),
+    youtubeKey = require('./config.json').youtubeKey,
     providerName = require('./package.json').name;
 
 class MusicProvider {
@@ -19,12 +20,20 @@ class MusicProvider {
         if (!asset.classInvertedIconUrl) {
             asset.classInvertedIconUrl = `${url}/provider/asset/${providerName}/true/icon`;
         }
-        if (!asset.backgroundImageUrl) {
-            let youtubeId = asset.url.substr(asset.url.length - 11);
 
+        let youtubeId = asset.url.substr(asset.url.length - 11);
+        let promises = [];
+        if (!asset.attributes.name) {
+            let p = rp.get({url: `https://www.googleapis.com/youtube/v3/videos?part=id%2Csnippet&id=${youtubeId}&key=${youtubeKey}`}).then(result => {
+                asset.attributes.name = result.items[0].snippet.title;
+            });
+            promises.push(p);
+        }
+
+        if (!asset.backgroundImageUrl) {
             let url = `${URI_BEGINNING}${youtubeId}/maxresdefault.jpg`;
 
-            return rp.get({
+            let p = rp.get({
                 url
             }).then(() => {
                 asset.backgroundImageUrl = url;
@@ -67,8 +76,11 @@ class MusicProvider {
                     throw err;
                 }
             }).catch(console.error);
+
+            promises.push(p);
         }
-        return Promise.resolve(asset);
+
+        return Promise.all(promises).then(() => asset);
     }
 }
 
